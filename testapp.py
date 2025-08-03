204069fce0b5902dced76e5b9706d59ffbf6f648
SubbaRao-Gadamsetty <subbarao.gadamsetty@gmail.com>
2025-08-03 11:22:23 +0530

    pytest code changes updated

--- testapp.py
+++ testapp.py
@@ -5,3 +5,10 @@ def test_home():
 
     assert response.status_code==200
     assert response.data== b"API is up and running!"
+
+
+def test_training():
+    response=app.test_client().get("/train")
+
+    assert response.status_code==200
+    assert response.data==b"Training Successful!"
